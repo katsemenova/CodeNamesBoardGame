ff371c365dc8dfe673e54b2d7be429b7b1291c98
Aaron <Aaron@S510p>
2018-02-20 15:37:28 -0500

    Test for clue legal or illegal

---
+++ src/ClueLegalityTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+/*
+ * @author Aaron & Hollis
+ */
+public class ClueLegalityTest {
+	@Test
+	public void testClueLegality(){
+		ClueLegality a = new ClueLegality();
+		String[][] board = {{"log"},{"tree"},{"leaf"}};
+		a.add("log");
+		ClueLegality b = new ClueLegality();
+		b.add("wood");
+		
+	
+	assertEquals("Your turn is forfeited!",a.equals(board),board);
+	assertEquals("You can let your field operatives pick a codename.",board);
+	
+		
+
+}
+}
