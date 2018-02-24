6571facd3ff6be55cd4d2709b61532a4d1f500bb
hopau <hopau@DESKTOP-563VS7E>
2018-02-24 12:57:03 -0500

    Added X and Y coordinates to Location object, added getters/setters

--- src/code/Location.java
+++ src/code/Location.java
@@ -5,9 +5,13 @@ public class Location {
 	// @author  Kateryna Semenova
 	private String agentName;
 	private int agentType; //0 Blue, 1 Red, 2 Assasin, 4 Bystander
+	private int xCoord;
+	private int yCoord;
 	private boolean visible;
 	
-	public Location(String codename, int agentType){
+	public Location(int x, int y, String codename, int agentType){
+		setxCoord(x);
+		setyCoord(y);
 		agentName = codename;
 		this.agentType = agentType;
 		visible = false;
@@ -29,6 +33,22 @@ public class Location {
 
 	public void setVisible(boolean visible) {
 		this.visible = visible;
+	}
+
+	public int getxCoord() {
+		return xCoord;
+	}
+
+	public void setxCoord(int xCoord) {
+		this.xCoord = xCoord;
+	}
+
+	public int getyCoord() {
+		return yCoord;
+	}
+
+	public void setyCoord(int yCoord) {
+		this.yCoord = yCoord;
 	}
 	
 	
