f599b7895d88bc20cdcd00208bf3f3f0d6da110f
Aaron <akkong@buffalo.edu>
2018-02-27 16:07:27 -0500

    fixed test

--- src/tests/ClueLegalityTest.java
+++ src/tests/ClueLegalityTest.java
@@ -1,21 +1,44 @@
 package tests;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
+
+import code.Board;
 
 /*
  * @author Aaron & Hollis
  */
 public class ClueLegalityTest {
 	@Test
-	public void testClueLegality(){
-		ClueLegality a = new ClueLegality();
-		String[][] board = {{"log"},{"tree"},{"leaf"}};
-		a.add("log");
-		ClueLegality b = new ClueLegality();
-		b.add("wood");
+	public void legaClueTestAgentName(){
+				
+		Board b = new Board();
 		
-	
-		assertEquals("Your turn is forfeited!",a.equals(board),board);
-		assertEquals("You can let your field operatives pick a codename.",board);
+		List<String> agentNames = b.getCodenamesUsed();
+		
+		assertEquals(false, b.legalClue(agentNames.get(1)));
+		assertEquals("Agent is not revealed yet, so using their name is not legal ",false, b.legalClue(agentNames.get(0)));
+
 
 }
+	@Test
+	public void legalClueEmptyStringTest(){
+		Board b = new Board();
+		assertEquals(false, b.legalClue(""));
+	}
+	
+	@Test
+	public void legalClueVisibleNameTest(){
+		Board b = new Board();
+		List<String> agentNames = b.getCodenamesUsed();
+		b.getLocations()[1][1].setVisible(true);
+		String testName = b.getLocations()[1][1].getPerson().getAgentName();
+		assertEquals(true,b.legalClue(testName));
+		
+		
+		
+	}
+
 }
