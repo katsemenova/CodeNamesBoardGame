e717b16f9e2edc0c772c079e3983b70bf3f5643e
xiaopingyang <xiaopingyang@10.84.169.111>
2018-03-01 14:35:00 -0500

    Test the creation of the location and visibility.

--- src/tests/RedTeamMoveFirstTest.java
+++ src/tests/RedTeamMoveFirstTest.java
@@ -13,8 +13,8 @@ import code.Person;
 
 public class RedTeamMoveFirstTest {
 	@Test
-	public void RedMoveFirst(){
-		Game game = new Game();
+	public void redMoveFirst(){
+		Game game = new Game(); 
 		assertEquals("Red team moves first.",1,game.getTurn());
 		
 	}
@@ -24,8 +24,6 @@ public class RedTeamMoveFirstTest {
 		Board b = game.getBoard();
 		Location[][] locations=b.getLocations();
 		int index=0;
-		boolean hidden=true;
-		
 		for (int i = 0; i < locations.length; i++) {
 			for (int j = 0; j < locations[0].length; j++) {
 				Location loc = locations[i][j];
@@ -39,17 +37,4 @@ public class RedTeamMoveFirstTest {
 		}
 		assertEquals("The are not 25 locations initialized",25,index);
 	}
-	
-//		Location a=new Location(0, 0, null);
-//		int person = 0;
-	//	boolean hidden = true; /*true means not revealed, false means revealed */
-	//	for(int i=0;i<b.locations.length;i++){/*for 25 instances */
-	//		b.locations.getAgentName();
-	//	}
-		
-//		assertEquals("AFRICA",a.getAgentName());
-	//	assertEquals(0,a.getAgentType());
-	//	assertTrue(hidden,a.isVisible());
-//	}
-
 }
