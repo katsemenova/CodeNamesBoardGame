a5bbc6a8963a741e97a156107a7f216a86743932
Kat Semenova <katya.semenova1405@gmail.com>
2018-03-01 13:31:24 -0500

    added more asserEquals line 47

--- src/tests/WinningStateTest.java
+++ src/tests/WinningStateTest.java
@@ -20,9 +20,8 @@ public class WinningStateTest {
 			for(int n=0;n<5;n++){
 				Location loc =arr[i][n];
 				if(loc.isVisible()&&loc.getPerson().getAgentType()==2) {//if location is visible and it's an assassin.
-					assassinExists = true;
-					
-			}
+					assassinExists = true;	
+				}
 			}
 		}
 		
@@ -41,11 +40,21 @@ public class WinningStateTest {
 				if(loc.getPerson().getAgentType()==2) {//if location is visible and it's an assassin.
 					loc.setVisible(true);
 					assassinExists = true;	
+					
 				}
 			}
 		}
-		
+		if(assassinExists){
+			if(game.getWinner() == "Red")
+				assertEquals("The game determined the winner incorrectly. the winner shouldn't be the person who revealed the assassin", true, 1!= game.getTurn());
+			else
+				assertEquals("The game determined the winner incorrectly. the winner shouldn't be the person who revealed the assassin", true, 0!= game.getTurn());
+				
+		}
+			
 		assertEquals("checkForAssassin should return false as no assassin exists", true, board.checkForAssassin());
+		
+		
 	}
 	@Test
 	public void allAgentsRevealedTestNoWinner(){
