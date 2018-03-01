2b5cdbc0a91e88e3ebb0c250b75121381ceabc82
hopau <hopau@DESKTOP-563VS7E>
2018-03-01 14:01:21 -0500

    added tests to ensure that team that revealed assassin doesn't win

--- src/tests/WinningStateTest.java
+++ src/tests/WinningStateTest.java
@@ -43,14 +43,6 @@ public class WinningStateTest {
 				}
 			}
 		}
-
-//		if(assassinExists){
-//			if(game.getWinner() == "Red")
-//				assertEquals("The game determined the winner incorrectly. the winner shouldn't be the person who revealed the assassin", true, 1!= game.getTurn());
-//			else
-//				assertEquals("The game determined the winner incorrectly. the winner shouldn't be the person who revealed the assassin", true, 0!= game.getTurn());
-//				
-//		}
 			
 		assertEquals("checkForAssassin should return false as no assassin exists", true, board.checkForAssassin());
 	}
@@ -99,7 +91,46 @@ public class WinningStateTest {
 		assertEquals("The winner should be red","Red",game.getWinner());
 	}
 	
-	//test when assassin is revealed on red's turn, blue MUST win
-	//test when assassin revealed on blue's turn, red MUST win
+	@Test
+	public void testAssassinRevealedByRed() {
+		Board b = new Board();
+		Game game = new Game();
+		Location[][] arr = b.getLocations();
+		
+		for(int i=0;i<5;i++){
+			for(int n=0;n<5;n++){
+				Location loc = arr[i][n];
+				if(loc.getPerson().getAgentType()==2) {//if location is visible and it's an assassin.
+					loc.setVisible(true);
+				}
+			}
+			game.setTurn(1); // set turn to Red
+		
+		
+		assertEquals("The game determined the winner incorrectly. the winner shouldn't be the Red Team who revealed the assassin", true, "Red" != game.getWinner());
+				
+		}
+	}
+	
+	@Test
+	public void testAssassinRevelaedByBlueWinner() {
+		Board b = new Board();
+		Game game = new Game();
+		Location[][] arr = b.getLocations();
+		
+		for(int i=0;i<5;i++){
+			for(int n=0;n<5;n++){
+				Location loc = arr[i][n];
+				if(loc.getPerson().getAgentType()==2) {//if location is visible and it's an assassin.
+					loc.setVisible(true);
+				}
+			}
+			game.setTurn(1); // set turn to Blue
+		
+		
+		assertEquals("The game determined the winner incorrectly. the winner shouldn't be the Blue Team who revealed the assassin", true, "Blue" != game.getWinner());
+				
+		}
+	}
 
 }
